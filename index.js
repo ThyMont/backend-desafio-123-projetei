10fbdb6fbf967b89669f2dd430c978e9fa916430
Thyago Monteiro <tmacademico@gmail.com>
2021-06-21 16:55:42 -0300

    Ajustes no index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+require('./src/database');
 const express = require('express');
-<<<<<<< HEAD
-const pfRouter = require('./routes/PessoaFisicaRouter');
+const routes = require('./src/routes');
+const cors = require('cors');
 const app = express();
 const port = 3001;
 
-app.use('/pf', pfRouter);
+app.use(express.json());
+app.use(cors());
+app.use('/pf', routes);
 app.get('/', (req, res) =>
   res.json({ message: 'Acesse a rota /pf para listar todos os resultados' })
 );
-=======
-const app = express();
-const port = 3001;
-
-app.get('/', (req, res) => res.json({ message: 'Hello world' }));
->>>>>>> parent of 8942d28 (Criação do Model e das rotas principais do Router de Pessoa Fisica)
 app.listen(port);
