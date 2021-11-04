3b3b7fa89c4027feeade2813836bf2b8c01fda07
Thyago Monteiro <48232753+ThyMont@users.noreply.github.com>
2021-11-04 17:09:15 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
-  dialect: 'mariadb',
-  host: 'db4free.net',
+  // dialect: 'mariadb',
+  // host: 'db4free.net',
+  dialect: 'mysql',
+  host: 'sql10.freemysqlhosting.net',
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
